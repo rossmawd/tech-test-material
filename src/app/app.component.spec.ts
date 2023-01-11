d9402b714e7d76ba24cb9d5eca21157ad888ef46
Ross Mawdsley <rossmawd@gmail.com>
2023-01-11 17:45:27 +0000

    deleted invalid test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TowerService } from './simulation/tower.service';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,6 +13,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        TowerService
+      ]
     }).compileComponents();
   });
 
@@ -26,10 +31,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('tech-test-material');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tech-test-material app is running!');
-  });
 });

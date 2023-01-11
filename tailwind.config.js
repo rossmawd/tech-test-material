e09b3ea6be359b964b9f3bbb59f2b0e7f84f18da
Ross Mawdsley <rossmawd@gmail.com>
2023-01-11 19:29:38 +0000

    installed and configured TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}", "./src/app/components/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

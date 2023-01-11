821ee839a5f571b078a7d3b0b871e62c62b407cb
Ross Mawdsley <rossmawd@gmail.com>
2023-01-11 19:06:31 +0000

    generated the header and added MaterialUI modules to app.module.ts

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+## How to Run
+
+...
+
+## Thought Process
+
+### Testing
+
+- I've chosen to remove the Karma test runner as I have more experience with Jest, and I believe it is faster to work with
+
 ## Shortcuts
 
 - I've opted to use the Tailwind CSS framework and the Material component library to speed up development
+
+- I used the AngularCLI to generate components (`ng g c <name>`) to prevent silly mistakes and save time
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TowerService } from './simulation/tower.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatTableModule } from '@angular/material/table'
+import { MatBadgeModule } from '@angular/material/badge'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,
+    MatListModule
   ],
   providers: [
     TowerService
